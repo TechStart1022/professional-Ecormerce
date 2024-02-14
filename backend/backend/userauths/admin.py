045ea7c43b655d2bbd5cf3a9c9790aa91625b243
whitesnow1022127 <snow1022127@gmail.com>
2024-02-14 23:39:36 +1000

    register

--- backend/backend/userauths/admin.py
+++ backend/backend/userauths/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from userauths.models import User,Profile
 
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['full_name','email','phone']
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['full_name','gender','country']
+    list_editable = ['gender','country']
+
 admin.site.register(User)
 admin.site.register(Profile)
+
+
+
 # Register your models here.
---
+++ backend/backend/userauths/serializer.py
@@ -0,0 +1,55 @@
+from rest_framework import serializers
+from rest_framework_simplejwt.tokens import Token
+from userauths.models import Profile, User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth.password_validation import validate_password
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True,validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        field = ['full_name','email','phone','password','password2']
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"Password","Not match password"})
+        return attrs
+    def create(self,validated_data):
+        user = User.objects.create(
+            full_name = validated_data['full_name'],
+            email = validated_data['email']
+            phone = validated_data['phone']
+        )
+        email_user, mobile = user.email.split('@')
+        user.set_password(validate_password['password'])
+
+        return user
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['full_name'] = user.full_name
+        token['email'] = user.email
+        token['username'] = user.username
+
+        try:
+            token['vendor_id'] = user.vendor_id
+        except:
+            token['vendor_id'] = 0
+        return token
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response['user'] = UserSerializer(instance.user).data
+        return response
